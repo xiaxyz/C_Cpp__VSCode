0fc0aa3de66ec1f3fdb84fc9f1a96d6ffe516920
xiax <3138199975@qq.com>
2023-03-05 17:33:29 +0800

    master

--- STUDY/Temp/test.cpp
+++ STUDY/Temp/test.cpp
@@ -2,92 +2,50 @@
 
 using namespace std;
 
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution
 {
 public:
-    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
+    ListNode *removeNthFromEnd(ListNode *head, int n)
     {
-        while (nums1.size() + nums2.size() > 2)
+        ListNode *Temp = head, *Walk = head;
+        for (int i = 0; i < n - 1; ++i)
         {
-            if (nums1.size() == 0)
-            {
-                nums2.erase(nums2.begin());
-                nums2.pop_back();
-            }
-            else if (nums2.size() == 0)
-            {
-                nums1.erase(nums1.begin());
-                nums1.pop_back();
-            }
-            else
-            {
-                if (nums1.front() < nums2.front())
-                {
-                    nums1.erase(nums1.begin());
-                }
-                else
-                {
-                    nums2.erase(nums2.begin());
-                }
-                if (nums2.size() != 0 && nums1.back() < nums2.back())
-                {
-                    nums2.pop_back();
-                }
-                else
-                {
-                    nums1.pop_back();
-                }
-            }
+            Temp = Temp->next;
         }
-        if (nums1.size() == 2)
+        while (Temp->next != nullptr)
         {
-            return 1.0 * (nums1[0] + nums1[1]) / 2;
+            Temp = Temp->next;
+            Walk = Walk->next;
         }
-        else if (nums2.size() == 2)
+        if (Walk->next == nullptr)
         {
-            return 1.0 * (nums2[0] + nums2[1]) / 2;
+            if (Walk == head)
+            {
+                head = nullptr;
+            }
+            delete Walk;
         }
-        else if (nums1.size() == 1)
+        else
         {
-            if (nums2.size() == 1)
-            {
-                return 1.0 * (nums1[0] + nums2[0]) / 2;
-            }
-            else
-            {
-                return nums1[0];
-            }
+            Walk->val = Walk->next->val;
+            ListNode *Dele = Walk->next;
+            Walk->next = Walk->next->next;
+            delete Dele;
         }
-        else if (nums2.size() == 1)
-        {
-            if (nums1.size() == 1)
-            {
-                return 1.0 * (nums2[0] + nums1[0]) / 2;
-            }
-            else
-            {
-                return nums2[0];
-            }
-        }
-        return 0;
-        // nums1.insert(nums1.end(), nums2.begin(), nums2.end());
-        // sort(nums1.begin(), nums1.end());
-        // if (nums1.size() % 2 == 0)
-        // {
-        //     return 1.0 * (nums1[nums1.size() / 2 - 1] + nums1[nums1.size() / 2]) / 2;
-        // }
-        // else
-        // {
-        //     return nums1[nums1.size() / 2];
-        // }
+        return head;
     }
 };
 int main(int argc, char **argv)
 {
-    vector<int> a{2,2,2,2}, b{2,2,2};
     Solution S;
-    string T;
-    cin >> T;
-    cout << S.findMedianSortedArrays(a, b) << T;
     return 0;
 }
